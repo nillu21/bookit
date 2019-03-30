879df5b10663698b9e06dc95e398783b85fe76e7
nilst <nitirs@ttu.ee>
2019-03-30 13:50:04 +0200

    Andmebaasis todo tabel

--- src/main/resources/db/migration/V1_1_0__my_first_migration.sql
+++ src/main/resources/db/migration/V1_1_0__my_first_migration.sql
@@ -16,3 +16,17 @@ INSERT INTO user (name, age)
 VALUES ('raiko', 15);
 INSERT INTO user (name, age)
 VALUES ('reiko', 15);
+
+CREATE TABLE todo (
+    id   long         NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    name varchar(255) NOT NULL,
+    description  varchar(255) NOT NULL
+    done bit NOT NULL
+);
+
+INSERT INTO todo(name,description,done)
+VALUES ('soomine', 'tahan burxi', 0);
+INSERT INTO todo(name,description,done)
+VALUES ('joomine', 'tahan viina', 0);
+INSERT INTO todo(name,description,done)
+VALUES ('tantsimine', 'tahan tantsida', 0);
