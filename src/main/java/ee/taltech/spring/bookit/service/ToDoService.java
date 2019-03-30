1286cd00c422daaee837193f0e79a9ae02dec6a6
nilst <nitirs@ttu.ee>
2019-03-30 14:23:45 +0200

    basik kontroller ja servise

---
+++ src/main/java/ee/taltech/spring/bookit/service/ToDoService.java
@@ -0,0 +1,23 @@
+package ee.taltech.spring.bookit.service;
+
+import ee.taltech.spring.bookit.domain.Todo;
+import ee.taltech.spring.bookit.repository.TodoRepository;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
+import java.util.List;
+
+@Service
+public class ToDoService {
+    @Resource
+    TodoRepository repo;
+
+    public Todo getToDoById(long id) {
+
+        return repo.getOne(id);
+    }
+
+    public List<Todo> getToDos() {
+        return repo.findAll();
+    }
+}
